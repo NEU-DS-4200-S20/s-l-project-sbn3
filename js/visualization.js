cf3a6586962149f0db2114992958670ff63bfb24
Prisca Joseph <priscajoseph@Priscas-MBP.fios-router.home>
2020-04-04 16:03:15 -0400

    meh the start of vis

--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,48 @@
 // Immediately Invoked Function Expression to limit access to our 
 // variables and prevent 
-((() => {
+var width = 960;
+var height = 500;
 
-  console.log("Hello, world!");
+var svg = d3
+  .select("#map-container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-})());
+var projection = d3
+  .geoAlbersUsa()
+  .translate([width / 2, height / 2])
+  .scale(width);
+
+var path = d3.geoPath().projection(projection);
+
+d3.json("us.json", function(us) {
+  //Error
+  d3.csv("data/cities-visited.csv", function(cities) {
+    drawMap(us, cities);
+  });
+});
+
+function drawMap() {
+  var mapGroup = svg.append("g").attr("class", "mapGroup");
+
+  mapGroup
+    .append("g")
+    // .attr("id", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", "states");
+
+    mapGroup
+    .append("path")
+    .datum(
+      topojson.mesh(us, us.objects.states, function(a, b) {
+        return a !== b;
+      })
+    )
+    .attr("id", "state-borders")
+    .attr("d", path);
+}
